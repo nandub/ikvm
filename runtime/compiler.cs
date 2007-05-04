b3ae5af7c02f1466752f05a1a287e53b75664fd0
Jeroen Frijters <jfrijters@users.sourceforge.net>
2007-05-04 06:41:46 +0000

    Fixed verifier/compiler to support dup_x2 form 2. Found by Derby test suite. Thanks to Albert Strasheim.

--- runtime/compiler.cs
+++ runtime/compiler.cs
@@ -131,7 +131,6 @@ class Compiler
 	private static MethodInfo getTypeFromHandleMethod;
 	private static MethodInfo monitorEnterMethod;
 	private static MethodInfo monitorExitMethod;
-	private static MethodInfo keepAliveMethod;
 	private static MethodWrapper getClassFromTypeHandle;
 	private static TypeWrapper java_lang_Object;
 	private static TypeWrapper java_lang_Class;
@@ -152,14 +151,12 @@ class Compiler
 	private LocalBuilder[] tempLocals = new LocalBuilder[32];
 	private Hashtable invokespecialstubcache;
 	private bool debug;
-	private bool keepAlive;
 
 	static Compiler()
 	{
 		getTypeFromHandleMethod = typeof(Type).GetMethod("GetTypeFromHandle", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(RuntimeTypeHandle) }, null);
 		monitorEnterMethod = typeof(System.Threading.Monitor).GetMethod("Enter", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(object) }, null);
 		monitorExitMethod = typeof(System.Threading.Monitor).GetMethod("Exit", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(object) }, null);
-		keepAliveMethod = typeof(System.GC).GetMethod("KeepAlive", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(object) }, null);
 		java_lang_Object = CoreClasses.java.lang.Object.Wrapper;
 		java_lang_Throwable = CoreClasses.java.lang.Throwable.Wrapper;
 		cli_System_Object = DotNetTypeWrapper.GetWrapperFromDotNetType(typeof(System.Object));
@@ -236,11 +233,6 @@ class Compiler
 		if(m.LineNumberTableAttribute != null && classLoader.EmitStackTraceInfo)
 		{
 			this.lineNumbers = new LineNumberTableAttribute.LineNumberWriter(m.LineNumberTableAttribute.Length);
-		}
-		if(ReferenceEquals(mw.Name, StringConstants.INIT))
-		{
-			MethodWrapper finalize = clazz.GetMethodWrapper(StringConstants.FINALIZE, StringConstants.SIG_VOID, true);
-			keepAlive = finalize != null && finalize.DeclaringType != java_lang_Object;
 		}
 
 		Profiler.Enter("MethodAnalyzer");
@@ -1508,61 +1500,6 @@ class Compiler
 				}
 			}
 
-			if(keepAlive)
-			{
-				// JSR 133 specifies that a finalizer cannot run while the constructor is still in progress.
-				// This code attempts to implement that by adding calls to GC.KeepAlive(this) before return,
-				// backward branches and throw instructions. I don't think it is perfect, you may be able to
-				// fool it by calling a trivial method that loops forever which the CLR JIT will then inline
-				// and see that control flow doesn't continue and hence the lifetime of "this" will be
-				// shorter than the constructor.
-				switch(instr.NormalizedOpCode)
-				{
-					case NormalizedByteCode.__return:
-					case NormalizedByteCode.__areturn:
-					case NormalizedByteCode.__ireturn:
-					case NormalizedByteCode.__lreturn:
-					case NormalizedByteCode.__freturn:
-					case NormalizedByteCode.__dreturn:
-						ilGenerator.Emit(OpCodes.Ldarg_0);
-						ilGenerator.Emit(OpCodes.Call, keepAliveMethod);
-						break;
-					case NormalizedByteCode.__if_icmpeq:
-					case NormalizedByteCode.__if_icmpne:
-					case NormalizedByteCode.__if_icmple:
-					case NormalizedByteCode.__if_icmplt:
-					case NormalizedByteCode.__if_icmpge:
-					case NormalizedByteCode.__if_icmpgt:
-					case NormalizedByteCode.__ifle:
-					case NormalizedByteCode.__iflt:
-					case NormalizedByteCode.__ifge:
-					case NormalizedByteCode.__ifgt:
-					case NormalizedByteCode.__ifne:
-					case NormalizedByteCode.__ifeq:
-					case NormalizedByteCode.__ifnonnull:
-					case NormalizedByteCode.__ifnull:
-					case NormalizedByteCode.__if_acmpeq:
-					case NormalizedByteCode.__if_acmpne:
-					case NormalizedByteCode.__goto:
-						if(instr.Arg1 <= 0)
-						{
-							ilGenerator.Emit(OpCodes.Ldarg_0);
-							ilGenerator.Emit(OpCodes.Call, keepAliveMethod);
-						}
-						break;
-					case NormalizedByteCode.__athrow:
-					case NormalizedByteCode.__athrow_no_unmap:
-					case NormalizedByteCode.__lookupswitch:
-					case NormalizedByteCode.__tableswitch:
-						if(ma.GetLocalTypeWrapper(i, 0) != VerifierTypeWrapper.UninitializedThis)
-						{
-							ilGenerator.Emit(OpCodes.Ldarg_0);
-							ilGenerator.Emit(OpCodes.Call, keepAliveMethod);
-						}
-						break;
-				}
-			}
-
 			switch(instr.NormalizedOpCode)
 			{
 				case NormalizedByteCode.__getstatic:
@@ -1647,39 +1584,11 @@ class Compiler
 					switch(classFile.GetConstantPoolConstantType(constant))
 					{
 						case ClassFile.ConstantType.Double:
-						{
-							double v = classFile.GetConstantPoolConstantDouble(constant);
-							if(v == 0.0 && BitConverter.DoubleToInt64Bits(v) < 0)
-							{
-								// FXBUG the x64 CLR JIT has a bug [1] that causes "cond ? -0:0 : 0.0" to be optimized to 0.0
-								// This bug causes problems for the sun.misc.FloatingDecimal code, so as a workaround we obfuscate the -0.0 constant.
-								// [1] https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=276714
-								ilGenerator.Emit(OpCodes.Ldc_I8, Int64.MinValue);
-								ilGenerator.Emit(OpCodes.Call, typeof(BitConverter).GetMethod("Int64BitsToDouble"));
-							}
-							else
-							{
-								ilGenerator.Emit(OpCodes.Ldc_R8, v);
-							}
+							ilGenerator.Emit(OpCodes.Ldc_R8, classFile.GetConstantPoolConstantDouble(constant));
 							break;
-						}
 						case ClassFile.ConstantType.Float:
-						{
-							float v = classFile.GetConstantPoolConstantFloat(constant);
-							if(v == 0.0 && BitConverter.DoubleToInt64Bits(v) < 0)
-							{
-								// FXBUG the x64 CLR JIT has a bug [1] that causes "cond ? -0:0 : 0.0" to be optimized to 0.0
-								// This bug causes problems for the sun.misc.FloatingDecimal code, so as a workaround we obfuscate the -0.0 constant.
-								// [1] https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=276714
-								ilGenerator.Emit(OpCodes.Ldc_I8, Int64.MinValue);
-								ilGenerator.Emit(OpCodes.Call, typeof(BitConverter).GetMethod("Int64BitsToDouble"));
-							}
-							else
-							{
-								ilGenerator.Emit(OpCodes.Ldc_R4, v);
-							}
+							ilGenerator.Emit(OpCodes.Ldc_R4, classFile.GetConstantPoolConstantFloat(constant));
 							break;
-						}
 						case ClassFile.ConstantType.Integer:
 							ilGenerator.LazyEmitLdc_I4(classFile.GetConstantPoolConstantInteger(constant));
 							break;
