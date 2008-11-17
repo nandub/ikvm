d04d50cf58809cac2a9fc37d4d7e6b1dc03e27d4
Jeroen Frijters <jfrijters@users.sourceforge.net>
2008-11-17 07:17:31 +0000

    Changed proxy stub name mangling to work around ildasm bug.

--- runtime/DynamicClassLoader.cs
+++ runtime/DynamicClassLoader.cs
@@ -26,7 +26,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
+#if IKVM_REF_EMIT
+using IKVM.Reflection.Emit;
+#else
 using System.Reflection.Emit;
+#endif
 
 namespace IKVM.Internal
 {
@@ -43,6 +47,7 @@ namespace IKVM.Internal
 
 	sealed class DynamicClassLoader : TypeWrapperFactory
 	{
+		// note that MangleNestedTypeName() assumes that there are less than 16 special characters
 		private static readonly char[] specialCharacters = { '\\', '+', ',', '[', ']', '*', '&', '\u0000' };
 		private static readonly string specialCharactersString = new String(specialCharacters);
 #if !STATIC_COMPILER
@@ -251,19 +256,23 @@ namespace IKVM.Internal
 				int index = specialCharactersString.IndexOf(c);
 				if(c == '.')
 				{
-					sb.Append("%-");
+					sb.Append("_");
+				}
+				else if(c == '_')
+				{
+					sb.Append("^-");
 				}
 				else if(index == -1)
 				{
 					sb.Append(c);
-					if(c == '%')
+					if(c == '^')
 					{
 						sb.Append(c);
 					}
 				}
 				else
 				{
-					sb.Append('%').AppendFormat("{0:X2}", index);
+					sb.Append('^').AppendFormat("{0:X1}", index);
 				}
 			}
 			return sb.ToString();
