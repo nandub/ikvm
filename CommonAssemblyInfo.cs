54d6eeb37cfc67cb52c7a9af562a6846a2d1e9c4
Jeroen Frijters <jfrijters@users.sourceforge.net>
2010-07-07 13:31:06 +0000

    0.44 rc 0 version.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeroen Frijters")]
 [assembly: AssemblyProduct("IKVM.NET")]
-[assembly: AssemblyCopyright("Copyright (C) 2002-2009 Jeroen Frijters")]
+[assembly: AssemblyCopyright("Copyright (C) 2002-2010 Jeroen Frijters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.39.3363.0")]
+[assembly: AssemblyVersion("0.44.0.0")]
 
 #if SIGNCODE
 	#pragma warning disable 1699
