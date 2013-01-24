76309e252c1cd80077685b8640e84724efeb4bbc
Jeroen Frijters <jfrijters@users.sourceforge.net>
2013-01-24 13:54:19 +0000

    Added AttributeHelper.SetInternalsVisibleToAttribute().

--- runtime/DynamicClassLoader.cs
+++ runtime/DynamicClassLoader.cs
@@ -92,7 +92,7 @@ namespace IKVM.Internal
 				if (!friends.Contains(name))
 				{
 					friends.Add(name);
-					((AssemblyBuilder)moduleBuilder.Assembly).SetCustomAttribute(new CustomAttributeBuilder(typeof(System.Runtime.CompilerServices.InternalsVisibleToAttribute).GetConstructor(new Type[] { typeof(string) }), new object[] { name }));
+					AttributeHelper.SetInternalsVisibleToAttribute((AssemblyBuilder)moduleBuilder.Assembly, name);
 				}
 			}
 		}
@@ -300,7 +300,7 @@ namespace IKVM.Internal
 				}
 				if(unloadableContainer == null)
 				{
-					unloadableContainer = moduleBuilder.DefineType("__<Unloadable>", TypeAttributes.Interface | TypeAttributes.Abstract);
+					unloadableContainer = moduleBuilder.DefineType(UnloadableTypeWrapper.ContainerTypeName, TypeAttributes.Interface | TypeAttributes.Abstract);
 					AttributeHelper.HideFromJava(unloadableContainer);
 				}
 				type = unloadableContainer.DefineNestedType(TypeNameUtil.MangleNestedTypeName(name), TypeAttributes.NestedPrivate | TypeAttributes.Interface | TypeAttributes.Abstract);
